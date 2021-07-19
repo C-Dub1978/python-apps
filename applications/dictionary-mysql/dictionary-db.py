ace84ac98ec73a58bba0f72658f8f2adbd67de32
TransientCW <chris@myubercode.com>
2021-07-19 14:35:18 -0600

    db query functionality working, need to format output and account for multiple rows returned from 1 search term

--- applications/dictionary-mysql/dictionary-db.py
+++ applications/dictionary-mysql/dictionary-db.py
@@ -1,5 +1,7 @@
 import mysql.connector
 import os
+import difflib
+from difflib import SequenceMatcher
 
 file = os.path.abspath('../files/.env')
 with open(file) as dataFile:
@@ -13,6 +15,36 @@ def printResults(results):
   print(results)
   print()
 
+def lookup(key):
+  if key.upper() in allWords:
+    return runInput(key.upper())
+  if key in allWords:
+    return runInput(key)
+  else:
+    matches = difflib.get_close_matches(key, allWords)
+    caseInsensitive = False
+    foundKey = ""
+
+    if len(matches) > 0:
+      for match in matches:
+        if str(match).upper() == key.upper():
+          caseInsensitive = True
+          foundKey = str(match)
+          break
+      if caseInsensitive == True:
+        print(foundKey)
+        return runInput(foundKey)
+      else:
+        confirmation = input('No matches for the word you entered, did you mean: ' + matches[0] + '?\nPress 1 for yes, 2 for no: ')
+        if confirmation == '1':
+          return runInput(matches[0])
+        elif confirmation == '2':
+          return 'No matches found'
+        else:
+          return 'No matches found'
+    else:
+      return 'No matches found for the word you entered, please check the spelling and try again'
+
 try:
   # Create connection
   connection = mysql.connector.connect(
@@ -23,16 +55,19 @@ try:
   )
   cursor = connection.cursor()
   ws = cursor.execute("SELECT word FROM dictionary")
-  allWords = cursor.fetchall()
-  print('ALL WORDS')
-  print(allWords)
+  words = cursor.fetchall()
+  allWords = []
+  for w in words:
+    word = str(w)
+    stripped = str(word[2:len(word) - 3])
+    allWords.append(stripped)
 
   while True:
     word = input('Enter word to search for , or type stop to exit the program: ')
     if word.upper() == 'STOP':
       break
     else:
-      results = runInput(word)
+      results = lookup(word)
       if results is not None:
         printResults(results)
       else:
